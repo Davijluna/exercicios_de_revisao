59c9eb6bd11e595353ade3dcaf8ba668745e4ba6
davi <davijluna@gmail.com>
2023-03-16 23:07:31 -0300

    feat: implementando componente Button na aplicação

--- Gerador_de_senhas_aleatorias/gerasenhasaleatorias/src/App.js
+++ Gerador_de_senhas_aleatorias/gerasenhasaleatorias/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { nanoid } from 'nanoid'
+import Button from './Button';
 
 const App = () => {
 
   const randomPassword = nanoid()
-  return <div>{randomPassword}</div>;
+  return (
+    <div>
+      <h1> senha: {randomPassword}</h1>
+      <Button />
+    </div>
+  )
 };
 
 export default App;
