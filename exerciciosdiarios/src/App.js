2d3dd2c595c2ea16f05490579aae2e68aefd98f8
davi <davijluna@gmail.com>
2023-03-17 17:03:44 -0300

    feat: adicionando componente Footer a aplicação

--- exerciciosdiarios/src/App.js
+++ exerciciosdiarios/src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
 const App = () => {
   return (
     <div>
       Meu App
+      <Header />
+      <Footer />
     </div>
   );
 }
