aedba751a3535252c9e502027bae9e084e2e58b6
davi <davijluna@gmail.com>
2023-03-18 00:47:58 -0300

    feat: criando um componente dentro de outro componente

--- exerciciosdiarios/src/App.js
+++ exerciciosdiarios/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Footer from "./Footer";
+import Form from "./Form/Form";
 import Header from "./Header";
+
+const Teste = () => {
+  const active = true;
+  if (active) {
+    return <p>Teste</p>;
+  }
+}
 
 const App = () => {
   return (
     <div>
       Meu App
+      <Teste />
       <Header />
+      <Form />
       <Footer />
     </div>
   );
