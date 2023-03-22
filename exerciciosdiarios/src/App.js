348c1e400a841379157a15809d458e7bf803d892
davi <davijluna@gmail.com>
2023-03-22 16:48:03 -0300

    feat: importando componente Form para componete App

--- exerciciosdiarios/src/App.js
+++ exerciciosdiarios/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
+import Form from "./Form/Form";
 
 // COM DESESTRUTURAÇÃO.
 //UTILIZANDO CHILDREN.
-const Titulo = ({cor, texto, children}) => {
-  return <h1 style={{color: cor}}>{texto}, {children}</h1>
-} 
+
 
 const App = () => {
   return (
     <div>
-      <Titulo cor="red" texto="Meu titulo 1"> ESSE É UM CHILDREN </Titulo>
-      <Titulo cor="blue" texto="Meu titulo 2" />
-      <Titulo cor="green" texto="Meu titulo 3" />
       Meu App
+      <Form />
     </div>
   );
 }
