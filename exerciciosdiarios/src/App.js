62cf4d67492f702ec14c8495f558a83d04f25a96
davi <davijluna@gmail.com>
2023-03-22 15:35:03 -0300

    feat: usando as propriedades props para mudar estilos das fontes

--- exerciciosdiarios/src/App.js
+++ exerciciosdiarios/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+const Titulo = (props) => {
+  return <h1 style={{color: props.cor}}>{props.texto}</h1>
+} 
 
 const App = () => {
   return (
     <div>
+      <Titulo cor="red" texto="Meu titulo 1" />
+      <Titulo cor="blue" texto="Meu titulo 2" />
+      <Titulo cor="green" texto="Meu titulo 3" />
       Meu App
     </div>
   );
