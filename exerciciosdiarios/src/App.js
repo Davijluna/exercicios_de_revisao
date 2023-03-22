be5994f2fd1ce0766d23f89d2b85af68e267fed9
davi <davijluna@gmail.com>
2023-03-22 16:27:53 -0300

    feat: utilizando children na aplicação

--- exerciciosdiarios/src/App.js
+++ exerciciosdiarios/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 // COM DESESTRUTURAÇÃO.
-const Titulo = ({cor, texto}) => {
-  return <h1 style={{color: cor}}>{texto}</h1>
+//UTILIZANDO CHILDREN.
+const Titulo = ({cor, texto, children}) => {
+  return <h1 style={{color: cor}}>{texto}, {children}</h1>
 } 
 
 const App = () => {
   return (
     <div>
-      <Titulo cor="red" texto="Meu titulo 1" />
+      <Titulo cor="red" texto="Meu titulo 1"> ESSE É UM CHILDREN </Titulo>
       <Titulo cor="blue" texto="Meu titulo 2" />
       <Titulo cor="green" texto="Meu titulo 3" />
       Meu App
