18122e1295da70abc84a9dfdf541e3632b82056d
davi <davijluna@gmail.com>
2023-03-22 16:22:27 -0300

    feat: utilizando desestruturação

--- exerciciosdiarios/src/App.js
+++ exerciciosdiarios/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Titulo = (props) => {
-  return <h1 style={{color: props.cor}}>{props.texto}</h1>
+// COM DESCINTRUTURAÇÃO.
+const Titulo = ({cor, texto}) => {
+  return <h1 style={{color: cor}}>{texto}</h1>
 } 
 
 const App = () => {
@@ -16,3 +17,21 @@ const App = () => {
 }
 
 export default App;
+
+// SEM DESCINTRUTURAÇÃO.
+// const Titulo = (props) => {
+//   return <h1 style={{color: props.cor}}>{props.texto}</h1>
+// } 
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Titulo cor="red" texto="Meu titulo 1" />
+//       <Titulo cor="blue" texto="Meu titulo 2" />
+//       <Titulo cor="green" texto="Meu titulo 3" />
+//       Meu App
+//     </div>
+//   );
+// }
+
+// export default App;
