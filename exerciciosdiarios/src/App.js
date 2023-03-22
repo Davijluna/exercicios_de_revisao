8cc8fba6d2c10c1182be3c886052189574f96d7f
davi <davijluna@gmail.com>
2023-03-22 15:26:15 -0300

    refactor: fazendo alterações para proximo exercicio de propiedades

--- exerciciosdiarios/src/App.js
+++ exerciciosdiarios/src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
 
-const Teste = () => {
-  const active = true;
-  if (active) {
-    return <p>Teste</p>;
-  }
-}
 
 const App = () => {
   return (
     <div>
       Meu App
-      <Teste /> 
     </div>
   );
 }
