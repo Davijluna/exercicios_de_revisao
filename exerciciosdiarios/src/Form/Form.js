232bc0461bb1de7439834e1014d21ff5bb6dcc3f
davi <davijluna@gmail.com>
2023-03-22 16:46:42 -0300

    feat: criando componetes para usar childrens na apicação

---
+++ exerciciosdiarios/src/Form/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+import Input from './Input'
+
+function Form() {
+  return (
+    <div>
+      <Input />
+      <Input />
+      <Button />
+    </div>
+  )
+}
+
+export default Form
