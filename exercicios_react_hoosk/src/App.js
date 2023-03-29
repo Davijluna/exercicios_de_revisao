2aafa98af2c7f2b12b1b360ed8d354459174e629
davi <davijluna@gmail.com>
2023-03-29 14:58:24 -0300

    feat: criando botão e inicioando as mudanças de estado de true para false

--- exercicios_react_hoosk/src/App.js
+++ exercicios_react_hoosk/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-     <h1>Hello World!!! </h1>
-    </div>
+function App() { 
+  const ativo = true;
+
+  return (<button disabled={!ativo}>{ativo ? 'Ativo' :'Inativo'}</button>
   );
 }
 
