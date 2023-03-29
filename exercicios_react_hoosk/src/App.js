4601eecbeaccefba78fd6711161021287b88cf4a
davi <davijluna@gmail.com>
2023-03-29 16:21:32 -0300

    refactor: adicionando comentarios para melhor compreensão do exericio

--- exercicios_react_hoosk/src/App.js
+++ exercicios_react_hoosk/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 function App() { 
+  // Criamos estados para a ativação do botão
   const ativoHook = React.useState(false);
   const ativoValor = ativoHook[0];
   const atualizaValor = ativoHook[1];
   console.log(ativoValor);
 
 function handleClinck () {
+  // com o paramentro "!ativoValor" ele sempre muda de false para true ou true para false.
   atualizaValor(!ativoValor);
 }
 
