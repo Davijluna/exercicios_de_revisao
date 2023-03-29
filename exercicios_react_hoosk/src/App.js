276edd216367b2ca2a55f1b78396f9f9db9c2a9b
davi <davijluna@gmail.com>
2023-03-29 16:30:18 -0300

    feat: desestruturando para ter o parametro do useState e a funcão e adicionando os comentarios para facilitar a comprensão

--- exercicios_react_hoosk/src/App.js
+++ exercicios_react_hoosk/src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 
 function App() { 
   // Criamos estados para a ativação do botão
-  const ativoHook = React.useState(false);
+  // Desestruturando o que têm no "React.useState" pois ele é como um array.
+  const [ativo, setAtivo] = React.useState(false);
   const ativoValor = ativoHook[0];
   const atualizaValor = ativoHook[1];
   console.log(ativoValor);
 
 function handleClinck () {
   // com o paramentro "!ativoValor" ele sempre muda de false para true ou true para false.
-  atualizaValor(!ativoValor);
+  setAtivo(!ativo);
 }
 
-  return (<button onClick={handleClinck}>{ativoValor ? 'Ativo' :'Inativo'}</button>
+  return (<button onClick={handleClinck}>{ativo ? 'Ativo' :'Inativo'}</button>
   );
 }
 
