75aa465feb411784ea38af0b16648e7fd60af99f
davi <davijluna@gmail.com>
2023-03-29 17:17:21 -0300

    refactor: adicionando comentario

--- exercicios_react_hoosk/src/App.js
+++ exercicios_react_hoosk/src/App.js
@@ -4,14 +4,24 @@ function App() {
   // Criamos estados para a ativação do botão
   // Desestruturando o que têm no "React.useState" pois ele é como um array.
   const [ativo, setAtivo] = React.useState(false);
+  const [dados, setDados] = React.useState({nome: "Davi", idade: "35"})
   console.log(ativo);
 
 function handleClinck () {
   // com o paramentro "!ativoValor" ele sempre muda de false para true ou true para false.
   setAtivo(!ativo);
+  // adicionando iinformações para o setDados 
+  setDados({...dados, faculdade: 'Possui faculdade'})
 }
 
-  return (<button onClick={handleClinck}>{ativo ? 'Ativo' :'Inativo'}</button>
+  return (
+    <div>
+      <p>{dados.nome}</p>
+      <p>{dados.idade}</p>
+      <p>{dados.faculdade}</p>
+        <button onClick={handleClinck}>{ativo ? 'Ativo' :'Inativo'}</button>
+    </div>
+
   );
 }
 
