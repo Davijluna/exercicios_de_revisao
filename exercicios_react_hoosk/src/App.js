e42a364e0daea1f83d02a6f95ff0595d7ea2b9f7
davi <davijluna@gmail.com>
2023-04-02 17:58:21 -0300

    feat:adicionando alterações de estados para mudanças corretas com setItems

--- exercicios_react_hoosk/src/App.js
+++ exercicios_react_hoosk/src/App.js
@@ -4,10 +4,16 @@ import Modal from "./Modal";
 
 function App() { 
   const [modal, setModal] = React.useState(false);
+  const [items, setItems] = React.useState('teste');
 
+  function handleClick() {
+    // items = 'Outros'; esta parte deve usar o setIems para alterar o estado. 
+    setItems('Outros')
+  }
 
   return (
     <div>
+      <button onClick={handleClick}>{items}</button>
       <Modal modal={modal} setModal={setModal} />
       <ButtonModal setModal={setModal} />
     </div>
