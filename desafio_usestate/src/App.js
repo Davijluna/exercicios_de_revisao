748d8106e73b8ae9ae602436917924ba501e3ca4
davi <davijluna@gmail.com>
2023-04-06 15:35:46 -0300

    fix: commit de comentário

--- desafio_usestate/src/App.js
+++ desafio_usestate/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Smartophone from "./Componentes/Smartophone";
 import Notebook from "./Componentes/Notebook";
 import Table from "./Componentes/Tablet";
-
+// tentar fazer essa parte antes das 10 da noite 
 function App() {
   return (
     <div>
