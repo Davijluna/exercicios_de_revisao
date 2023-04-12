79fd00a5f42911bfb3b0d6132a028081bfc4eceb
davi <davijluna@gmail.com>
2023-04-12 11:43:58 -0300

    feat: fazendo o teste na API mas ainda sem sucesso

--- desafio_usestate/src/App.js
+++ desafio_usestate/src/App.js
@@ -2,8 +2,19 @@ import React from "react";
 import Produto from "./Produto";
 
 const App =() => {
+  const [dados, setDados] = React.useState(null);
+
+  async function handleclick(event) {
+   // eslint-disable-next-line no-template-curly-in-string
+   const response = await fetch('https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}');
+    console.log(response);
+  }
+
   return (
     <div>
+      <button style={{margin: '5px'}} onClick={handleclick} >notebook</button>
+      <button style={{margin: '5px'}}  onClick={handleclick} >Smartphone</button>
+      <button style={{margin: '5px'}}  onClick={handleclick} >Tablet</button>
       <Produto />
     </div>
   );
--- desafio_usestate/src/Produto.js
+++ desafio_usestate/src/Produto.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 const Produto = () => {
   return (
-    <div>
-      <button>Smartphone</button>
-      <button>Notebook</button>
-      <button>Smartphone</button>
+    <div>test
     </div>
   )
 }
