156e499e81583ce9913361563d2c15d04e4875b7
davi <davijluna@gmail.com>
2023-04-12 16:40:30 -0300

    refactor: acrescentando comentatios

--- desafio_usestate/src/App.js
+++ desafio_usestate/src/App.js
@@ -2,19 +2,22 @@ import React from "react";
 import Produto from "./Produto";
 
 const App =() => {
+  // estado de dados da API
   const [dados, setDados] = React.useState(null);
+  //estado que altera para true quando está carregando e altera pra false quando para de carregar.
   const [carregando, setCarregando] = React.useState(null);
 
   async function handleclick(event) {
-
+  // inicializa como true pra mostrar o carregamento.
   setCarregando(true);
 
   const response = await fetch(
     `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
   );
-  // transformando a constante response em um arquivo json
+  // transformando a constante response em um arquivo json.
   const json = await response.json();
     setDados(json);
+  // depois dos dados garregados muda pra false o garregamento.
     setCarregando(false);
   }
 
--- desafio_usestate/src/Produto.js
+++ desafio_usestate/src/Produto.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Produto = ({ dados }) => {
   return (
+    // recebendo os dados e imprimindo da tela as mensagens que recebeu da API.
     <div>
       <h1>{dados.nome}</h1>
       <p>R$ {dados.preco}</p>
