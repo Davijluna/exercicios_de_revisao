1c9d05415c41ae13338f5fc66d02fd8442bc38fa
davi <davijluna@gmail.com>
2023-04-12 16:24:53 -0300

    feat: terminando de fazer o fetch com useState

--- desafio_usestate/src/App.js
+++ desafio_usestate/src/App.js
@@ -3,8 +3,11 @@ import Produto from "./Produto";
 
 const App =() => {
   const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
 
   async function handleclick(event) {
+
+  setCarregando(true);
 
   const response = await fetch(
     `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
@@ -12,6 +15,7 @@ const App =() => {
   // transformando a constante response em um arquivo json
   const json = await response.json();
     setDados(json);
+    setCarregando(false);
   }
 
   return (
@@ -19,7 +23,8 @@ const App =() => {
       <button style={{margin: '5px'}} onClick={handleclick} >notebook</button>
       <button style={{margin: '5px'}}  onClick={handleclick} >Smartphone</button>
       <button style={{margin: '5px'}}  onClick={handleclick} >tablet</button>
-      {dados && <Produto dados={dados} />}
+      {carregando && <p>Carregando</p>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   );
 }
