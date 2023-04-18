95f083a47d96a7844affb66bbf1c32a7d7a88018
davi <davijluna@gmail.com>
2023-04-18 10:43:33 -0300

    feat:adiquirindo informações da API de pokemons

--- API_pokemon/app.js
+++ API_pokemon/app.js
@@ -1,14 +1,22 @@
 const fetchPokemon  = () => {
   const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-for (let i = 1; i <= 150; i += 1) {
-  fetch(getPokemonUrl(i)).then(response => response.json())
-}
-// está linha de baixo será retirada 
-  fetch(url)
-  .then(response => response.json())
-  .then(pokemon => {
-    console.log(pokemon)
+  const pokemonPromises = []
+
+  for (let i = 1; i <= 150; i += 1) {
+    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+    
+  }
+
+  Promise.all(pokemonPromises)
+  .then(pokemosns => {
+    console.log(pokemosns)
+
+    const lisPokemons = pokemosns.reduce((accumulator, pokemon) => {
+      accumulator += `<li>${pokemon.name}</li>`
+      return accumulator
+    }, '')
+    // console.log(lisPokemons)
   })
 }
 
